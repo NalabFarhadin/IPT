4d483d7d290382ff7b89ac605ffefa7edd399c62
NalabFarhadin <144081956+NalabFarhadin@users.noreply.github.com>
2025-10-23 09:24:44 +0800

    Add files via upload

---
+++ Nalab_IPT101_WPF/Nalab_IPT101_WPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nalab_IPT101_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void SaveMedicine_Click(object sender, RoutedEventArgs e)
+        {
+            string name = MedicineNameTextBox.Text.Trim();
+            string dosage = DosageTextBox.Text.Trim();
+
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(dosage))
+            {
+                MessageBox.Show("Please enter both medicine name and dosage.");
+                return;
+            }
+
+            MedicineListBox.Items.Add($"{name} - {dosage}");
+
+            MedicineNameTextBox.Clear();
+            DosageTextBox.Clear();
+        }
+    }
+}
