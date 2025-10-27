c0a05e32f6419fc622c43f8221f762e4a284e3d6
NalabFarhadin <144081956+NalabFarhadin@users.noreply.github.com>
2025-10-27 20:55:12 +0800

    Add files via upload

---
+++ NALABSOLUTION/Medicine/Medicine Procedures/CreateMedicine.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[Create]
+	@MedicineID NVARCHAR(10) = NULL, 
+    @MedicineName NVARCHAR(10) = NULL, 
+    @BrandName NVARCHAR(10) = NULL, 
+    @Category NVARCHAR(10) = NULL
+AS
+BEGIN
+    INSERT INTO [dbo].[Medicine] ([MedicineID], [MedicineName], [BrandName], [Category])
+    VALUES (@MedicineID, @MedicineName, @BrandName,@Category)
+END
---
+++ NALABSOLUTION/Medicine/Table/Medicine.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Medicine]
+(
+	[MedicineID] NVARCHAR(10) NOT NULL PRIMARY KEY, 
+    [MedicineName] NVARCHAR(50) NULL, 
+    [BrandName] NVARCHAR(50) NULL, 
+    [Category] NVARCHAR(10) NULL
+)
